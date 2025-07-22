c289f2cfc53313a80ea90b439a81a60e087fc887
TonyP1992 <okpetrakov@gmail.com>
2025-07-22 21:12:29 +0800

    Update lesson-4.js

--- js/lesson-4.js
+++ js/lesson-4.js
@@ -19,20 +19,97 @@
 // 19 - зроби це саме, але використовуй шаблонні рядки та метод insertAdjacentHTML()
 // 20 - очисти список
 
-
-const bodyEl = document.querySelector('body');
+const bodyEl = document.querySelector("body");
 console.log(bodyEl);
-const titleEl = document.querySelector('#title');
-console.log(titleEl);
-const listEl = document.querySelector('.list');
+const titleEl = document.querySelector("#title");
+// console.log(titleEl);
+const listEl = document.querySelector(".list");
 console.log(listEl);
-const dataTopicEls = document.querySelectorAll('data-topic');
-console.log(dataTopicEls);
+const dataTopicEls = document.querySelectorAll("data-topic");
+// console.log(dataTopicEls);
 firstTopicEl = dataTopicEls[0];
-console.log(firstTopicEl);
+// console.log(firstTopicEl);
 lastTopicEl = dataTopicEls[dataTopicEls.length - 1];
-console.log(lastTopicEl);
-H1Sibling = titleEl.nextElementSibling
-const allh3Els = document.querySelectorAll('h3');
-console.log(allh3Els);
-allh3Els.forEach()
+// console.log(lastTopicEl);
+H1Sibling = titleEl.nextElementSibling;
+const allh3Els = document.querySelectorAll("h3");
+// console.log(allh3Els);
+allh3Els.forEach((el) => {
+  el.classList.add("active");
+});
+
+const navTopicEl = document.querySelector('li[data-topic="navigation"]');
+console.log(navTopicEl);
+navTopicEl.style.background = "yellow";
+const navTopicText = navTopicEl.querySelector("p");
+navTopicText.textContent = "Я змінив тут текст!";
+const currentTopic = "manipulation";
+const topicEl = document.querySelector(`[data-topic="${currentTopic}"]`);
+topicEl.style.backgroundColor = "blue";
+const titleCompleted = document.querySelector(".completed");
+console.log(titleCompleted);
+
+if (titleCompleted) {
+  const parentLi = titleCompleted.closest("li");
+  if (parentLi) {
+    parentLi.remove();
+  }
+}
+
+const paragraph = document.createElement("p");
+paragraph.textContent = "Об'єктна модель документа (Document Object Model)";
+titleEl.insertAdjacentElement("afterend", paragraph);
+// _________________________________________________
+const list = document.querySelector("ul");
+
+// const newLi = document.createElement("li");
+
+// const h3 = document.createElement("h3");
+// h3.textContent = "Властивість innerHTML";
+
+// const p = document.createElement("p");
+// p.textContent =
+//   "Ще один спосіб створити DOM-елементи і помістити їх в дерево - це використовувати рядки з тегами і дозволити браузеру зробити всю важку роботу";
+
+// newLi.append(h3, p);
+
+// list.appendChild(newLi);
+
+// ______________________________________________
+
+const newItemHTML = `
+  <li>
+    <h3>Властивість innerHTML</h3>
+    <p>Ще один спосіб створити DOM-елементи і помістити їх в дерево - це використовувати рядки з тегами і дозволити браузеру зробити всю важку роботу</p>
+  </li>
+`;
+
+list.insertAdjacentHTML('beforeend', newItemHTML);
+
+// _______________________________________________
+
+list.innerHTML = '';
+
+// _________________________________________________
+
+const formEl = document.querySelector(".js-contact-form");
+
+formEl.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  //   const inputValue = event.target.elements.userName.value.trim();
+  //   const isCheck = event.target.elements.accept.checked;
+  const { userName, accept } = event.target.elements;
+  const inputValue = userName.value.trim();
+  const isCheck = accept.checked;
+
+  if (inputValue === "" || !isCheck) {
+    alert("Fill all fields")
+return;
+  }
+
+  console.log({ userName: inputValue });
+  formEl.reset();
+}
